8d98a635936d9d9b78df3ba248d9901b11d7643c
Grisha <hersheleh@gmail.com>
2012-10-27 18:39:13 -0700

    I added the accounts module to this install I believe, maybe something else. I'm not tottally sure

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, session
+from accounts.accounts import *
 
 app = Flask('__name__')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gj.db'
+app.secret_key = 'giragesandcatsaredabomb'
+
+app.register_blueprint(accounts, url_prefix='/edit')
+db.init_app(app)
+
 
 @app.route("/")
-def home():
+def index():
     return render_template('home.html')
 
 @app.route("/services")
@@ -15,6 +21,13 @@ def services():
 def about():
     return render_template("about.html")
 
+
+@app.route('/edit')
+def edit():
+    if 'username' in session:
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('accounts.login'))
 
 
 if __name__ == '__main__':
--- plug-test.py
+++
@@ -1,5 +0,0 @@
-from flask.views import View
-
-
-
-
